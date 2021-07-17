1dd6decf9401d3584193d19e0ce43f70400c2484
AmanPant777 <footballeramanpt65@gmail.com>
2021-07-17 14:17:11 +0530

    Make Home Navbar and Style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 import './App.css';
 import Login from './Comp/Login'
+import Header from './Comp/Header'
+import Home from './Comp/Home'
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route>
+          <Route exact path='/'>
             <Login/>
+          </Route>
+          <Route path='/home'>
+            <Header/>
+            <Home/>
           </Route>
         </Switch>
       </Router>
