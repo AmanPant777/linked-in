b76e19f40e4f0b6411c408147ca5d0d619861dde
AmanPant777 <footballeramanpt65@gmail.com>
2021-07-18 17:10:07 +0530

    Adding leftside to home

--- src/Comp/Home.jsx
+++ src/Comp/Home.jsx
@@ -1,11 +1,69 @@
 import React from 'react'
-
+import styled from 'styled-components'
+import LeftSide from './leftside'
+import Main from './main'
+import RightSide from './rightside'
 const Home = () => {
-    return (
-        <div>
-          Home  
-        </div>
-    )
+  return (
+    <Container>
+      <Section>
+        <h5><a >Hiring in a hurry</a></h5>
+        <p>Find talented pros in record time with upwork and keep buisness moving</p>
+      </Section>
+      <Layout>
+        <LeftSide />
+        <Main />
+        <RightSide />
+      </Layout>
+    </Container>
+  )
 }
 
 export default Home
+const Container = styled.div`
+padding: 52px;
+max-width: 100%;
+`
+const Section = styled.div`
+
+padding: 16px 0;
+min-height: 50px;
+box-sizing: content-box;
+text-align: center;
+display: flex;
+justify-content: center;
+align-items: center;
+h5{
+  color: #0a66c2;
+  font-size: 20px;
+  a{
+    font-weight: 700;
+  }
+}
+p{
+  font-size: 14px;
+  color: #43464a;
+  font-weight: 600;
+  margin-left: 10px;
+  @media (max-width:768px){
+ margin: 0;
+}
+}
+@media (max-width:768px){
+  flex-direction: column;
+  padding: 0 5px;
+}
+`
+const Layout = styled.div`
+display: grid;
+grid-template-areas: "leftside main rightside";
+grid-template-columns: minmax(0,5fr) minmax(0,10fr) minmax(300px,7fr) ;
+column-gap: 25px;
+row-gap: 25px;
+margin: 25px 0;
+@media (max-width:768px){
+  display: flex;
+  flex-direction: column;
+  padding: 0 5px ;
+}
+`
---
+++ src/Comp/leftside.jsx
@@ -0,0 +1,137 @@
+import React from 'react'
+import styled from 'styled-components'
+const Leftside = () => {
+    return (
+        <Container>
+            <ArtCard>
+                <UserInfo>
+                    <CardBackGround />
+                    <a >
+                        <Photo />
+                        <Link>Welcome ,there</Link>
+                    </a>
+                    <a >
+                        <AddPhotoText>Add a photo</AddPhotoText>
+                    </a>
+                </UserInfo>
+                <Widget>
+                    <a >
+                        <div>
+                            <span>Connections</span>
+                            <span>Grow Your Network</span>
+                        </div>
+                        <img src="/images/widget-icon.svg" alt="" />
+                    </a>
+                </Widget>
+                <Item>
+                    <span>
+                        <img src="/images/item-icon.svg" alt="" />
+                        My Items
+                    </span>
+                </Item>
+            </ArtCard>
+        </Container>
+    )
+}
+export default Leftside
+const Container = styled.div`
+grid-area: leftside;
+`
+const ArtCard = styled.div`
+
+text-align: center;
+overflow: hidden;
+margin-bottom: 8px;
+background-color: #fff;
+border-radius: 5px;
+transition: box-shadow 83ms;
+position: relative;
+box-shadow: 0 0 0 1px rgb(0 0 0/15%),0 0 0 rgb(0 0 0/20%);
+`
+const UserInfo = styled.div`
+border-bottom: 1px solid rgba(0,0,0,0.8);
+padding: 12px 12px 16px ;
+word-wrap: break-word;
+word-break: break-all;
+`
+const CardBackGround = styled.div`
+background-image:url('https://static-exp1.licdn.com/sc/h/9e0ckeb27mzi70ne80f4hv7il');
+background-position: center;
+background-size: 462px;
+height: 54px;
+margin: -12px -12px ;
+`
+const Photo = styled.div`
+box-shadow: none;
+background-image: url('https://media-exp3.licdn.com/dms/image/C5603AQF55L01PLk3cw/profile-displayphoto-shrink_100_100/0/1611205794563?e=1632355200&v=beta&t=0GHoXvFDFGEvzzDvcjC0igiTkfB07pc6wNfDi6lP-IE');
+width: 72px;
+height: 72px;
+box-sizing:border-box;
+background-clip: content-box;
+background-color: white;
+background-position: center;
+background-size: 60%;
+background-repeat: no-repeat;
+border: 2px solid white;
+margin: -38px auto 12px;
+border-radius: 50%;
+`
+const Link = styled.div`
+font-size: 16px;
+line-height: 1.5;
+color: rgba(0,0,0,0.9);
+font-weight: 600;
+`
+const AddPhotoText = styled.div`
+color: #0a66c2;
+margin-top: 4px;
+font-size: 12px;
+line-height: 1.33;
+font-weight: 400;
+`
+const Widget = styled.div`
+border-bottom: 1px solid rgba(0,0,0,0.25);
+padding-top: 12px;
+padding-bottom: 12px;
+&>a{
+ text-decoration: none;
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
+ padding: 4px 12px;
+ &:hover{
+     background-color: rgba(0,0,0,0.1);
+ }
+ div{
+     display: flex;
+     flex-direction: column;
+     text-align: left;
+     span{
+         font-size: 12px;
+         line-height: 1.33;
+         &::first-child{
+             color: rgba(0,0,0,0.6);
+         }
+         &::nth-child(2){
+             color: rgba(0,0,0,1);
+         }
+     }
+ }
+}
+`
+const Item = styled.a`
+background-color: rgba(0,0,0,0.08);
+text-align: left;
+padding: 12px;
+font-size:12px;
+display: block;
+margin: 5px;
+span{
+    display: flex;
+    align-items: center;
+    color: rgba(0,0,0,1);
+}
+&:hover{
+    background-color: rgba(0,0,0,0.08);
+}
+`
