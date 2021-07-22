85c8adebb963b739e891ac9001818ee0aabf3923
AmanPant777 <footballeramanpt65@gmail.com>
2021-07-22 12:37:58 +0530

    adding action to add data to firebase

--- src/redux/action/actions.js
+++ src/redux/action/actions.js
@@ -1,4 +1,4 @@
-import { auth, provider } from "../../firebase";
+import db, { auth, provider, storage } from "../../firebase";
 
 const setUser = (payload) => ({
   type: "SET_USER",
@@ -28,5 +28,42 @@ export const signOutApi = () => {
       .signOut()
       .then(() => dispatch(setUser(null)))
       .catch((error) => console.log(error.message));
+  };
+};
+
+export const postArticleAPI = (payload) => {
+  return (dispatch) => {
+    if (payload.image != "") {
+      const upload = storage
+        .ref(`images/${payload.image.name}`)
+        .put(payload.image);
+      upload.on(
+        "State_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Progress:${progress}%`);
+          if (snapshot.state === "RUNNING") {
+            console.log(`Progress:${progress}%`);
+          }
+        },
+        (error) => console.log(error.code),
+        async () => {
+          const downloadURL = await upload.snapshot.ref.getDownloadURL();
+          db.collection("articles").add({
+            actor: {
+              description: payload.user.email,
+              title: payload.user.displayName,
+              date: payload.timestamp,
+              image: payload.user.photoURL,
+            },
+            video: payload.video,
+            sharedImg: downloadURL,
+            comments: 0,
+            description: payload.description,
+          });
+        }
+      );
+    }
   };
 };
